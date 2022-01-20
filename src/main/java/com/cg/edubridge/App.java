805b5d70ae776e180da59cfdff8bb30197508408
Anandhimohan <97152553+Anandhimohan@users.noreply.github.com>
2022-01-19 20:28:43 -0800

    Add files via upload
    Criteria_Query

---
+++ src/main/java/com/cg/edubridge/App.java
@@ -0,0 +1,13 @@
+package com.cg.edubridge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
